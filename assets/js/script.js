fa15e0fc4cee5c2f999f1fd227de06b59aebd8d5
cpayment <cpayment12@gmail.com>
2023-12-13 19:38:06 -0600

    Simplified css, added button functionality to generate a random name onto the screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,23 @@ fetch('https://swapi.dev/api/people')
   })
   .then(function (data) {
     console.log(data);
+
+    var fetchRequest = data;
+
+    var buttonEl = document.getElementById('generate');
+    var wordEl = document.getElementById('fetch-word');
+
+    buttonEl.addEventListener('click', function(event) {
+
+      var num = getRandomInt(fetchRequest.results.length);
+
+      console.log(fetchRequest.results[num].name)
+
+      wordEl.innerHTML = fetchRequest.results[num].name;
+    })
+
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+    
   });
-
-const wordDisplayEl = document.getElementById('word-display');
-const generateButtonEl = document.getElementById('generate');
-
-const fetchWordValue = data;
-
-generateButtonEl.addEventListener('click', function(event) {
-
-    wordDisplayEl.innerHTML = fetchWordValue;
-})
