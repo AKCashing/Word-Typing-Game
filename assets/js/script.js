d79f395c318569102b56adc20d8128ed284d2784
cpayment <cpayment12@gmail.com>
2023-12-14 14:37:16 -0600

    Press 'Enter' to change the word displayed and clear the input field

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,32 @@
-fetch('https://swapi.dev/api/people')
+// Random number generator function
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Randomly grab a number(0-8)
+var getRandomPageNumber = getRandomInt(9);
+
+// Fetch the Star Wars API
+fetch('https://swapi.dev/api/people/?page='+ getRandomPageNumber)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
-
+    // Grab the requested list of people on the page
     var fetchRequest = data;
 
-    var buttonEl = document.getElementById('generate');
-    var wordEl = document.getElementById('fetch-word');
+    var userInputEl = document.getElementById('user-input');
+    var displayEl = document.getElementById('display');
 
-    buttonEl.addEventListener('click', function(event) {
+    // When the User presses 'Enter', change the word and clear the input field
+    userInputEl.addEventListener('keypress', function(event){
+      if (event.key === 'Enter'){
+        var getRandomNumber = getRandomInt(fetchRequest.results.length);
 
-      var num = getRandomInt(fetchRequest.results.length);
+        displayEl.innerHTML = fetchRequest.results[getRandomNumber].name;
 
-      console.log(fetchRequest.results[num].name)
-
-      wordEl.innerHTML = fetchRequest.results[num].name;
+        userInputEl.value = '';
+      }
     })
-
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * max);
-    }
-    
   });
