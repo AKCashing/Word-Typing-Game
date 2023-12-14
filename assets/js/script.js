6f24999a293a3de3827540b5db899c406cf210a1
cpayment <cpayment12@gmail.com>
2023-12-14 15:47:59 -0600

    Created a Homepage with simple instructions and a functional 'Start' button that will begin the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+// Grab the Homepage and Start-Button element ID's
+var homepageEl = document.getElementById('homepage');
+var startButtonEl = document.getElementById('start');
+
+// Grab the Gamepage, Display, and User-Input element ID's
+var gamepageEl = document.getElementById('gamepage');
+var displayEl = document.getElementById('display');
+var userInputEl = document.getElementById('user-input');
+
 // Random number generator function
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -13,11 +22,18 @@ fetch('https://swapi.dev/api/people/?page='+ getRandomPageNumber)
   })
   .then(function (data) {
     console.log(data);
-    // Grab the requested list of people on the page
+    // Grab the requested list of people on the API page
     var fetchRequest = data;
 
-    var userInputEl = document.getElementById('user-input');
-    var displayEl = document.getElementById('display');
+    // When the user clicks the button 'Start', begin the game
+    startButtonEl.addEventListener('click', function(event){
+      var getRandomNumber = getRandomInt(fetchRequest.results.length);
+
+      displayEl.innerHTML = fetchRequest.results[getRandomNumber].name;
+      
+      homepageEl.style.display = 'none';
+      gamepageEl.style.display = 'flex';
+    })
 
     // When the User presses 'Enter', change the word and clear the input field
     userInputEl.addEventListener('keypress', function(event){
